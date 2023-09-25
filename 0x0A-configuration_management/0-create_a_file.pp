b858230b573e0732f2b3bd4e22716f525a174017
danjor667 <jordannguepi@gmail.com>
2023-09-25 10:20:39 +0200

    fix:lint

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,9 @@
 #creating a file in /tmp
 
 file {'/tmp/school':
-  ensure     => present,
-  content    => 'I love Puppet',
-  group => 'www-data',
-  owner => 'www-data',
-  mode       => '0744'
+  ensure  => present,
+  content => 'I love Puppet',
+  group   => 'www-data',
+  owner   => 'www-data',
+  mode    => '0744'
   }
